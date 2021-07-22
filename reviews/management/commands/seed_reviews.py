3c8b4d77133b81fb68d02079551446edee24bd04
soyeonnnb <la28s5d@naver.com>
2021-07-23 04:47:22 +0900

    update recent progress

---
+++ reviews/management/commands/seed_reviews.py
@@ -0,0 +1,41 @@
+import random
+from django.core.management import BaseCommand
+from django_seed import Seed
+from reviews import models as review_models
+from users import models as user_models
+from rooms import models as room_models
+
+
+class Command(BaseCommand):
+
+    help = "This command creates reivews"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--number",
+            default=2,
+            type=int,
+            help="How many reivews do you want to create",
+        )
+
+    def handle(self, *args, **options):
+        number = options.get("number", 1)
+        seeder = Seed.seeder()
+        users = user_models.User.objects.all()
+        rooms = room_models.Room.objects.all()
+        seeder.add_entity(
+            review_models.Review,
+            number,
+            {
+                "accuracy": lambda x: random.randint(1, 5),
+                "community": lambda x: random.randint(1, 5),
+                "cleanlines": lambda x: random.randint(1, 5),
+                "location": lambda x: random.randint(1, 5),
+                "check_in": lambda x: random.randint(1, 5),
+                "value": lambda x: random.randint(1, 5),
+                "user": lambda x: random.choice(users),
+                "room": lambda x: random.choice(rooms),
+            },
+        )
+        seeder.execute()
+        self.stdout.write(self.style.SUCCESS(f"{number} reviews created!"))
---
+++ rooms/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.core.paginator import Paginator
+from . import models
+
+
+def all_rooms(request):
+    page = request.GET.get("page", 1)
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10)
+    rooms = paginator.get_page(page)
+    return render(request, "rooms/home.html", context={"pages": rooms})
