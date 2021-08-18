ad5fdbb1274d9d6a9e345f04cd4d90635344ef1b
soyeonnnb <la28s5d@naver.com>
2021-08-19 04:03:33 +0900

    feat: login & logout

--- config/settings.py
+++ config/settings.py
@@ -50,6 +50,7 @@ PROJECT_APPS = [
     "lists.apps.ListsConfig",
     "conversations.apps.ConversationsConfig",
 ]
+
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views import View
+from django.views.generic import FormView
+from django.urls import reverse_lazy, reverse
+from django.contrib.auth import authenticate, login, logout
+from . import forms
 
-# Create your views here.
+
+class LoginView(FormView):
+
+    template_name = "users/login.html"
+    success_url = reverse_lazy("core:home")
+    form_class = forms.LoginForm
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect(reverse("core:home"))
