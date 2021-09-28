d26acb8b736c2aa391236fa54f1374df0c2ecdde
soyeonnnb <la28s5d@naver.com>
2021-09-29 03:45:19 +0900

    feat : tailwindcss init

--- config/settings.py
+++ config/settings.py
@@ -132,7 +132,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/static/" # 이건 서버 url
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 AUTH_USER_MODEL = "users.User"
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 const gulp = require("gulp");
 
-const css = ()=>{
-    const postCSS = require("gulp-postcss");
-    const sass = require("gulp-sass");
-    const minify = require("gulp-csso");
-    sass.compiler = require("node-sass");
-    return gulp;
+const css = () => {
+  const postCSS = require("gulp-postcss");
+  var sass = require("gulp-sass")(require("sass"));
+  const minify = require("gulp-csso");
+  sass.compiler = require("node-sass");
+  return gulp
+    .src("assets/scss/styles.scss", { allowEmpty: true }) //파일 찾기
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postCSS([require("tailwindcss"), require("autoprefixer")])) //postcss 가 이해하는 플러그인. 폴더에 있는 코드들을 실제 css코드로 변환시켜줌
+    .pipe(minify()) // 코드를 짧게 만들어줌
+    .pipe(gulp.dest("static/css")); // 마지막에 결과물을 저장할 폴더
 };
 
 exports.default = css;
